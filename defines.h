8b2aea83e5806f1f1a65f8e75517407cf7bac187
srihari damacharla <sriharidamacharla@gmail.com>
2025-08-24 23:18:09 +0530

    Add files via upload

---
+++ defines.h
@@ -0,0 +1,28 @@
+                   /* defines.h */
+									 
+#ifndef __DEFINES_H__
+#define __DEFINES_H__
+
+#define SETBIT(WORD,BITPOS)            (WORD|=(1<<BITPOS))
+#define CLRBIT(WORD,BITPOS)            (WORD&=~(1<<BITPOS))
+#define CPLBIT(WORD,BITPOS)            (WORD^=(1<<BITPOS))
+#define WRITEBIT(WORD,BITPOS,BIT)      (BIT?SETBIT(WORD,BITPOS):CLRBIT(WORD,BITPOS))
+#define READBIT(WORD,BITPOS)           ((WORD>>BITPOS)&1)
+#define READWRITEBIT(WORD,WBIT,RBIT)   (WORD=((WORD&~(1<<WBIT))|(((WORD>>RBIT)&1)<<WBIT)))
+#define WRITEUNIBBLE(WORD,BITPOS,BYTE) (WORD=(WORD&~(0x0000000F<<BITPOS))|((BYTE>>4)<<BITPOS))
+#define WRITELNIBBLE(WORD,BITPOS,BYTE) (WORD=(WORD&~(0x0000000F<<BITPOS))|((BYTE&0X0F)<<BITPOS))
+#define READNIBBLE(WORD,BITPOS)        ((WORD>>BITPOS)&0x0000000F)
+#define WRITEBYTE(WORD,BITPOS,BYTE)    (WORD=(WORD&~(0x000000FF<<BITPOS))|(BYTE<<BITPOS))      
+#define READBYTE(WORD,BITPOS)          ((WORD>>BITPOS)&0x000000FF)
+
+#endif
+
+
+
+
+
+
+
+
+
+
