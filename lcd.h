d6a309a6304934ff5058d2a2d0d76401714e3b31
srihari damacharla <sriharidamacharla@gmail.com>
2025-08-24 23:26:03 +0530

    Add files via upload

---
+++ lcd.h
@@ -0,0 +1,9 @@
+void Write_CMD_LCD(char);
+void Write_DAT_LCD(char);
+void Write_LCD(char);
+void LCD_Init(void);
+void Write_str_LCD(char *);
+void Write_int_LCD(signed int);//signed int(2 byte)-> -32768 to 32767
+void Write_float_LCD(float f,char );
+//void SetCursor(unsigned char row, unsigned char col);
+void LCD_SetCursor(unsigned int row, unsigned char col);
